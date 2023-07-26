7e3d6677050e8f9a0bd7d0ca974355c9e17f4e1f
Creeper-boop <78140643+Creeper-boop@users.noreply.github.com>
2023-07-26 11:22:17 +0200

    duck

--- src/tui.rs
+++ src/tui.rs
@@ -155,7 +155,7 @@ pub trait TUI {
         sorted_elements.sort_by(|a, b| {
             let a_z = a.lock().unwrap().get_z();
             let b_z = b.lock().unwrap().get_z();
-            a_z.cmp(b_z)
+            a_z.cmp(&b_z)
         });
         for element in sorted_elements {
             let element_lock = element.lock().unwrap();
