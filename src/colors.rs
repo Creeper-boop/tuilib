63a411f270389ec8ccffbaa1844100b70cb2ea77
Creeper-boop <78140643+Creeper-boop@users.noreply.github.com>
2023-08-29 19:39:20 +0200

    color const update

--- src/colors.rs
+++ src/colors.rs
@@ -2,74 +2,86 @@
 #![allow(missing_docs)]
 use crate::tui::Color;
 
+pub const YELLOW: Color = Color {
+    r: 0xFF,
+    g: 0xCC,
+    b: 0x00,
+};
+
 pub const ORANGE: Color = Color {
     r: 0xFF,
     g: 0xB0,
     b: 0x00,
 };
 
-pub const GREY: Color = Color {
+pub const ORANGE_75: Color = Color {
+    r: 0xCC,
+    g: 0x91,
+    b: 0x0D,
+};
+
+pub const ORANGE_50: Color = Color {
+    r: 0x9A,
+    g: 0x73,
+    b: 0x1B,
+};
+
+pub const LIME: Color = Color {
+    r: 0x33,
+    g: 0xFF,
+    b: 0x00,
+};
+
+pub const GREEN: Color = Color {
+    r: 0x2D,
+    g: 0xE0,
+    b: 0x00,
+};
+
+pub const GREEN_75: Color = Color {
+    r: 0x2F,
+    g: 0xB5,
+    b: 0x0D,
+};
+
+pub const GREEN_50: Color = Color {
+    r: 0x31,
+    g: 0x8B,
+    b: 0x1B,
+};
+
+pub const WHITE: Color = Color {
+    r: 0xF6,
+    g: 0xF8,
+    b: 0xFF,
+};
+
+pub const LIGHT_GREY: Color = Color {
+    r: 0xDF,
+    g: 0xE3,
+    b: 0xED,
+};
+
+pub const LIGHT_GREY_75: Color = Color {
+    r: 0xB4,
+    g: 0xB7,
+    b: 0xBF,
+};
+
+pub const LIGHT_GREY_50: Color = Color {
+    r: 0x8A,
+    g: 0x8C,
+    b: 0x91,
+};
+
+pub const BLACK: Color = Color {
     r: 0x1F,
     g: 0x1F,
     b: 0x1F,
 };
-/*
-HashMap<&str, Color> = HashMap::from([
-    (
-        "fg",
-        Color {
-            r: 0xFF,
-            g: 0xB0,
-            b: 0x00,
-        },
-    ),
-    (
-        "fg_75",
-        Color {
-            r: 0xCC,
-            g: 0x91,
-            b: 0x0D,
-        },
-    ),
-    (
-        "fg_50",
-        Color {
-            r: 0x9A,
-            g: 0x73,
-            b: 0x1B,
-        },
-    ),
-    (
-        "fg_dark",
-        Color {
-            r: 0x35,
-            g: 0x35,
-            b: 0x35,
-        },
-    ),
-    (
-        "bg",
-        Color {
-            r: 0x1F,
-            g: 0x1F,
-            b: 0x1F,
-        },
-    ),
-    (
-        "bg_light",
-        Color {
-            r: 0x35,
-            g: 0x35,
-            b: 0x35,
-        },
-    ),
-    (
-        "bg_select",
-        Color {
-            r: 0xFF,
-            g: 0xCC,
-            b: 0x00,
-        },
-    ),
-]);
-*/
+
+pub const GREY: Color = Color {
+    r: 0x35,
+    g: 0x35,
+    b: 0x35,
+};
